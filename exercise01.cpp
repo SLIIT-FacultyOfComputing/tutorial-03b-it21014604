3a1c0726ccedf2951f96ca26fc11ad87d54620f9
Senarathna L.W.S.L {sajith} <87459703+it21014604@users.noreply.github.com>
2021-08-02 16:40:14 +0000

    I have written tutorial exercise 1

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
+#include<iomanip>
 using namespace std;
 int main() {
  float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
  char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
+ cout <<"0        1         2         3 "<<endl;
+ cout <<"123456789012345678901234567890 "<<endl;
+ cout << setw(5)<< setiosflags(ios::fixed)<<"No";
+ cout << setw(15)<< setiosflags(ios::fixed)<<"Name";
+ cout << setw(10)<< setiosflags(ios::fixed)<<"Marks"<<endl;
+
  for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
+     cout << setw(5)<< setiosflags(ios::fixed)<<r+1;
+     cout << setw(15)<< setiosflags(ios::fixed)<<names[r];
+     cout << setw(10)<< setprecision(2)<<marks[r]<<endl;
  }
 }
